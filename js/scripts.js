67f59a66a50a40dd8d40d0359a095a8399fb6a5c
Edyta Dudek <edyta.dudek@10g.pl>
2022-10-19 19:26:00 +0200

    colored checked and unchecked

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,12 @@ const item = document.querySelector('.main__list--item');
 
 item.addEventListener('click', (e)=>{
 
-    if(e.target.classList.contains('item')){
+    if(!e.target.classList.contains('checked')){
         e.target.classList.add('checked')
        }
+       else{
+        e.target.classList.remove('checked')
+       }
+    
+    
     });
