5a12577867bafc201a59ed8ada85904985036955
Edyta Dudek <edyta.dudek@10g.pl>
2022-10-20 10:00:55 +0200

    colored7

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,23 @@ const item = document.querySelector('.main__list--item');
 item.addEventListener('click', (e)=>{
 
     if(!e.target.classList.contains('checked')){
+      
         e.target.classList.add('checked')
-       }
-       else{
+     
+    }
+       
+    else{
+        
         e.target.classList.remove('checked')
-       }
-    
+      
+    }
     
     });
+
+
+    const list = document.querySelector('#product-list-in');
+    const products = input.value;
+   
+    const tab = [];
+
+
